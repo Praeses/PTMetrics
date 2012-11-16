6b2f8ed51ae8bd1b3c695fb6f6e4fc17729348fd
Steven D'Anna <steven.danna@gmail.com>
2012-11-16 13:47:18 -0600

    Moving database stuff around

---
+++ src/PTMetrics/Models/ProjectTimelineVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PTMetrics.Models
+{
+    public class ProjectTimelineVM
+    {
+        public string Categories { get; set; }  // Should look like 'cat1', 'cat2', cat3'
+    }
+}
